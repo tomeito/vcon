729ecb97ce203413215d398420705baa597c4c85
Taisei Nakajima <ne300085@senshu-u.jp>
2019-03-10 00:26:48 +0900

    entry_userのmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190305155949) do
+ActiveRecord::Schema.define(version: 20190309152242) do
+
+  create_table "entry_users", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "producer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "producers", force: :cascade do |t|
     t.string "name"
