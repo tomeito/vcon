96424713567561ef832a7bafdb87cabc76caef94
Taisei Nakajima <ne300085@senshu-u.jp>
2019-03-12 02:25:00 +0900

    多重保存をしなくなった

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,15 @@ notifier = Slack::Notifier.new(ENV["WEBHOOK_URL"])
 csv_answer = CSV.read('app/assets/form_answer.csv', headers: true)
 
 csv_answer.each do |data|
-  user = EntryUser.find_or_initialize_by(name: "#{data['参加キャラクター名']}",
-                                         description: "#{data['キャラクターのプロフィール(60文字程度でお願いします。)']}",
-                                         producer_id: "#{Producer.find_by(name: data['制作者の個人名・団体名'])}")
+  # p data['参加キャラクター名']
+  user = EntryUser.find_or_initialize_by(name: "#{data['参加キャラクター名']}")
   if user.new_record?
-    user.save!
-    # notifier.ping("新しい参加者だよ！\n名前:#{user.name}\n")
+    user.update_attributes(
+        name: "#{data['参加キャラクター名']}",
+        description: "#{data['キャラクターのプロフィール(60文字程度でお願いします。)']}",
+        producer_id: "#{Producer.find_by(name: data['制作者の個人名・団体名'])}"
+    )
+    notifier.ping("新しい参加者だよ！\n名前:#{user.name}\n")
     p "新しい参加者だよ！\n名前:#{user.name}"
   end
 end
