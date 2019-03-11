06b657797316891bdd1bd00d49b2ea676c6a8c68
Taisei Nakajima <ne300085@senshu-u.jp>
2019-03-12 02:00:44 +0900

    DBに入る値を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ csv_answer = CSV.read('app/assets/form_answer.csv', headers: true)
 
 csv_answer.each do |data|
   user = EntryUser.find_or_initialize_by(name: "#{data['参加キャラクター名']}",
-                                         description: "#{data['キャラクターのプロフィール(60文字程度でお願いします。)']},
-                                         producer_id: #{Producer.where(name: data['制作者の個人名・団体名'])}")
+                                         description: "#{data['キャラクターのプロフィール(60文字程度でお願いします。)']}",
+                                         producer_id: "#{Producer.find_by(name: data['制作者の個人名・団体名'])}")
   if user.new_record?
     user.save!
     # notifier.ping("新しい参加者だよ！\n名前:#{user.name}\n")
