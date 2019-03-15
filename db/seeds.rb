73d6ed71e688341364b079e22b7908969a5af703
Taisei Nakajima <ne300085@senshu-u.jp>
2019-03-15 23:14:43 +0900

    とりあえずseedからentry_userとproducer流し込みおわり

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,12 @@ notifier = Slack::Notifier.new(ENV["WEBHOOK_URL"])
 csv_answer = CSV.read('app/assets/form_answer.csv', headers: true)
 
 csv_answer.each do |data|
-  producer = Producer.find_or_initialize_by(name "#{data['制作者の個人名・団体名']}")
+  producer = Producer.find_or_initialize_by(name: "#{data['制作者の個人名・団体名']}")
   if producer.new_record?
     producer.update_attributes(
             name: "#{data['制作者の個人名・団体名']}",
-        comments: "#{data['出場に対して意気込みを一言']}"
-
+        comments: "#{data['出場に対して意気込みを一言']}",
+         contact: "#{data['SNS等へのリンク']}"
     )
   end
   # p data['参加キャラクター名']
