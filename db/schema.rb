be12852825285f96ceb61a80684d452080b3c121
Taisei Nakajima <ne300085@senshu-u.jp>
2019-03-20 02:30:39 +0900

    model作成おわり

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190316150134) do
+ActiveRecord::Schema.define(version: 20190319172707) do
 
   create_table "entry_users", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20190316150134) do
     t.datetime "updated_at", null: false
     t.boolean "is_top_image"
     t.index ["entry_user_id"], name: "index_images_on_entry_user_id"
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "vote_user_id"
+    t.integer "entry_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["entry_user_id"], name: "index_votes_on_entry_user_id"
+    t.index ["vote_user_id"], name: "index_votes_on_vote_user_id"
   end
 
 end
