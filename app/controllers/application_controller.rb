03345a44fb375381a8533b671f4068c46324e3f9
Taisei Nakajima <ne300085@senshu-u.jp>
2019-04-04 17:54:14 +0900

    cotrollerできたかもしれない

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?, :authenticate
 
-  private
+  protected
 
   def current_user
     return unless session[:user_id]
     @current_user ||= VoteUser.find_by(uid: session[:user_id])
   end
 
-  def logged_in?
-    !!session[:user_id]
-  end
-
   def authenticate
     return if logged_in?
     redirect_to root_path, alert: 'ログインしてください'
   end
+
+  private
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
 end
--- app/controllers/vote_user_controller.rb
+++ app/controllers/vote_user_controller.rb
@@ -1,7 +1,13 @@
 class VoteUserController < ApplicationController
 
   def update
+    if authenticate
+      @current_user = current_user
+      @current_user.voted_at = Time.now
+      @current_user.save
+      @vote = Vote.new(vote_user_id: "#{@current_user.id}", entry_user_id: "#{params[:entry_user_id]}")
 
+    end
   end
 
 end
