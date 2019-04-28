c8c7fc5a3426a52f3979a21f8188db40cc00169f
Taisei Nakajima <ne300085@senshu-u.jp>
2019-04-29 00:35:55 +0900

    一日一回のロジック書いた

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,12 @@
 class Vote < ApplicationRecord
   belongs_to :vote_user
   belongs_to :entry_user
+
+  def already_vote_today?(vote_user)
+    return true if Vote.where(vote_user_id: vote_user.id,
+                              created_at: Time.now.midnight..
+                                         (Time.now.midnight + 1.day))
+
+    false
+  end
 end
