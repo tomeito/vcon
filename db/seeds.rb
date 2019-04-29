1dd29ae4c2611d7f54f53a710b8304c379d42c4e
Taisei Nakajima <ne300085@senshu-u.jp>
2019-04-29 21:35:46 +0900

    seedでis_mr判定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,20 @@ csv_answer = CSV.read('app/assets/form_answer.csv', headers: true)
 
 csv_answer.each do |data|
   user = EntryUser.find_or_initialize_by(name: data['参加キャラクター名'])
-  if user.new_record?
-    user.update_attributes(
-               name: data['参加キャラクター名'],
-        description: data['キャラクターのプロフィール(60文字程度でお願いします。)'],
-           comments: data['出場に対して意気込みを一言'],
-        twitter_url: data['TwitterのURL'],
-    youtube_channel: data['YouTubeのチャンネルURL'],
-          other_url: data['その他SNSなどのリンク']
-    )
-    # notifier.ping("新しい参加者だよ！\n名前:#{user.name}\n")
-    puts "新しい参加者だよ！\n名前:#{user.name}"
-  end
+  is_mr = false
+  next unless user.new_record?
+
+  is_mr = true if data['どちらに参加されますか？'] == 'バーチャル美少年コンテスト'
+  user.update_attributes(
+             name: data['参加キャラクター名'],
+      description: data['キャラクターのプロフィール(60文字程度でお願いします。)'],
+         comments: data['出場に対して意気込みを一言'],
+      twitter_url: data['TwitterのURL'],
+  youtube_channel: data['YouTubeのチャンネルURL'],
+        other_url: data['その他SNSなどのリンク'],
+            is_Mr: is_mr
+  )
+  # notifier.ping("新しい参加者だよ！\n名前:#{user.name}\n")
+  puts "新しい参加者だよ！\n名前:#{user.name}"
 
 end
