04ad2ea14392458afaf30f52f7167a78b9ddd98b
taichanne30 <ne300209@senshu-u.jp>
2019-04-29 23:24:32 +0900

    ミス/ミスターで表示変えるやつ

--- app/controllers/entry_users_controller.rb
+++ app/controllers/entry_users_controller.rb
@@ -1,7 +1,17 @@
 class EntryUsersController < ApplicationController
 
   def index
-    @entry_users = EntryUser.all
+    puts request.fullpath
+    if request.fullpath == "/mr"
+      @contest_title = "バーチャルミスターコンテスト"
+      @entry_users = EntryUser.where(is_Mr: true)
+    elsif request.fullpath == "/ms"
+      @contest_title = "バーチャルミスコンテスト"
+      @entry_users = EntryUser.where(is_Mr: false)
+    else
+      @contest_title = "バーチャルミス・ミスターコンテスト"
+      @entry_users = EntryUser.all
+    end
   end
 
   def show
