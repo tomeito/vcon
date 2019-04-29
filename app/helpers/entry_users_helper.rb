1388a7ef52d735f9021c3b790229be9d2a307a83
taichanne30 <ne300209@senshu-u.jp>
2019-04-29 23:20:15 +0900

    銀桜/少年Sさんへの対応&リファクタリング

--- app/helpers/entry_users_helper.rb
+++ app/helpers/entry_users_helper.rb
@@ -1,2 +1,10 @@
 module EntryUsersHelper
+  def picture_dir_exist?(entry_user)
+    File.exist? picture_dir(entry_user)
+  end
+
+  private
+  def picture_dir(entry_user)
+    "#{Rails.root}/app/assets/images/#{entry_user.name.remove("/")}"
+  end
 end
