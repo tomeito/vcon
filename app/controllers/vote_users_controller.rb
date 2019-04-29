4047d12d39d14635dddc539908bf6373a23aa394
Taisei Nakajima <ne300085@senshu-u.jp>
2019-04-29 14:39:40 +0900

    votes#createに投票ロジック移した

--- app/controllers/vote_users_controller.rb
+++ app/controllers/vote_users_controller.rb
@@ -1,20 +1,2 @@
 class VoteUsersController < ApplicationController
-
-  def update
-    @current_user = VoteUser.find_by(id: params[:id])
-    if authenticate
-      @vote = Vote.new(vote_user_id: "#{@current_user.id}",
-                       entry_user_id: "#{params[:entry_user_id]}")
-      if already_vote_today? @current_user
-        flash[:notice] = '本日はすでに投票済みです。明日も投票をよろしくお願いいたします！'
-        redirect_back(fallback_location: root_path)
-      end
-      @vote.save
-    else
-      flash[:notice] = '投票するにはログインが必要です。'
-      redirect_to root_path
-    end
-  end
-
-
 end
