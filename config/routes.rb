277578d451f4727a596b67122394b66468e3e70a
Taisei Nakajima <ne300085@senshu-u.jp>
2019-04-29 22:15:28 +0900

    書き忘れ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/mr', to: 'entry_users#index'
   get '/ms', to: 'entry_users#index'
-  resource :vote_users, only: :create
+  resource :votes, only: :create
   resource :sessions, only: [:create, :destroy]
 end
