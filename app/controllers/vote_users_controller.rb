8b181871975cf0062ef8749cd7f93b1cca65de7c
Taisei Nakajima <ne300085@senshu-u.jp>
2019-04-29 22:10:38 +0900

    route再構成

--- app/controllers/vote_users_controller.rb
+++ app/controllers/vote_users_controller.rb
@@ -1,15 +1,3 @@
 class VoteUsersController < ApplicationController
 
-  def update
-    if authenticate
-      @current_user = current_user
-      update_vote_date @current_user
-      @vote = Vote.new(vote_user_id: "#{@current_user.id}", entry_user_id: "#{params[:entry_user_id]}")
-      @vote.save
-    else
-      redirect_to root_path
-    end
-  end
-
-
 end
