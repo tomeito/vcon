be5c742262cc55c861e8d8f1ab0d7647b44e8422
Taisei Nakajima <ne300085@senshu-u.jp>
2019-04-29 23:39:56 +0900

    ログイン確認できた

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     vote_user = VoteUser.find_or_create_from_twitter(request.env['omniauth.auth'])
     session[:user_id] = vote_user.uid
     flash[:notice] = 'ユーザ認証に成功しました。'
-    redirect_to root_path
+    redirect_to '/'
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :entry_users, only: [:index, :show]
-  resource :vote_users, only: [:create, :update]
+  resource :votes, only: :create
   resource :sessions, only: [:create, :destroy]
 
   get 'auth/twitter/callback' => 'sessions#create'
