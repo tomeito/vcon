7b493129a081e16075e5ff48f8d986f100c910e3
Taisei Nakajima <ne300085@senshu-u.jp>
2019-04-30 22:19:19 +0900

    投票チェックでミスミスター1回ずつ投票できるようにした

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,17 +1,15 @@
 class VotesController < ApplicationController
 
   def create
-    @current_user = VoteUser.find_by(id: params[:id])
+    @vote_user = VoteUser.find_by(id: params[:vote][:vote_user_id])
+    @entry_user = EntryUser.find_by(id: params[:vote][:entry_user_id])
     if authenticate
-      if already_vote_today? @current_user
+      if already_vote_today?(@vote_user, @entry_user.is_Mr)
         flash[:notice] = '本日はすでに投票済みです。明日も投票をよろしくお願いいたします！'
         redirect_back(fallback_location: root_path)
       end
-      Vote.create(vote_user_id: "#{@current_user.id}",
-                  entry_user_id: "#{params[:entry_user_id]}")
-    else
-      flash[:notice] = '投票するにはログインが必要です。'
-      redirect_to root_path
+      Vote.create(vote_user_id: @vote_user.id,
+                  entry_user_id: @entry_user.id)
     end
   end
 
