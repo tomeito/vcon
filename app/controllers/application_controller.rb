bc0d80fe44edb19dd301fc5ccd4e8ccdade4ab46
taichanne30 <ne300209@senshu-u.jp>
2019-05-01 14:29:45 +0900

    already_vote_todayの条件をunlessに変更 ※カラムに「ないとき」じゃなければtrue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   end
 
   def already_vote_today?(vote_user, is_mr)
-    return true if Vote.where(vote_user_id: vote_user.id,
+    return true unless Vote.where(vote_user_id: vote_user.id,
                                 created_at: Date.today.all_day,
                                 is_Mr: is_mr).blank?
 
