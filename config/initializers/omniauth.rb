a4cb5478d015966c8e5dff7cff0b15b7e6cf96c5
Taisei Nakajima <ne300085@senshu-u.jp>
2019-05-15 00:10:03 +0900

    OmniAuthにfull_urlを教えてあげた

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.full_host = 'https://virtual-con.toylabo.tech/'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
 end
