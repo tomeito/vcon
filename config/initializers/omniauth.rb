e41eadda382da9835dcebe06d819e673a2f01697
Taisei Nakajima <ne300085@senshu-u.jp>
2019-05-15 02:07:38 +0900

    sessionが消え去る対策

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.full_host = 'https://virtual-con.toylabo.tech/'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
+  provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'], {:provider_ignores_state => true}
 end
