055d283fa7306a37bc4c63cf0d182f5b9784713d
taichanne30 <ne300209@senshu-u.jp>
2019-05-21 11:02:06 +0900

    レイアウト修正20190521

--- app/helpers/entry_users_helper.rb
+++ app/helpers/entry_users_helper.rb
@@ -1,4 +1,29 @@
 module EntryUsersHelper
+
+  def other_side_name(is_mr)
+    if is_mr
+      "ミス"
+    else
+      "ミスター"
+    end
+  end
+
+  def other_side_index(is_mr)
+    if is_mr
+      "/ms"
+    else
+      "/mr"
+    end
+  end
+
+  def other_side_link(is_mr)
+    if is_mr
+      link_to "ミスの参加者一覧を見る", "/ms", class:"to_ms"
+    else
+      link_to "ミスターの参加者一覧を見る", "/mr", class:"to_mr"
+    end
+  end
+
   def picture_dir_exist?(entry_user)
     File.exist? picture_dir(entry_user)
   end
